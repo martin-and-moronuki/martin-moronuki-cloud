ad848956004f6a1c9d03495144845d3deb5b64e4
Chris Martin <ch.martin@gmail.com>
2020-09-07 03:27:16 -0600

    time

--- update.hs
+++ update.hs
@@ -9,13 +9,17 @@ import Data.Aeson.Encode.Pretty
 import qualified Data.Aeson.Encode.Pretty as AP
 import Data.Aeson.Optics
 import qualified Data.ByteString.Lazy as LBS
+import qualified Data.HashMap.Lazy as HashMap
 import qualified Data.Text as T
+import Data.Time.Calendar
+import Data.Time.Clock
+import Data.Time.Format
+import Data.Time.LocalTime
 import Network.HTTP.Simple
 import Network.HTTP.Types.Header
 import Optics
 import Relude
 import System.Process
-import qualified Data.HashMap.Lazy as HashMap
 
 main :: IO ()
 main = read file >>= either fail pure >>= traverse update >>= write file
@@ -31,7 +35,7 @@ write file = LBS.writeFile file . encodePretty' conf
     conf = AP.defConfig {AP.confIndent = AP.Spaces 2, AP.confTrailingNewline = True}
 
 update :: Value -> IO Value
-update = updateRev >=> updateHash
+update v = (updateRev >=> updateHash) v >>= \v' -> if v == v' then pure v' else updateTime v'
 
 updateRev :: Value -> IO Value
 updateRev x = setRev <$> getRev <*> pure x
@@ -76,6 +80,18 @@ gitHubResponseRev = forceKey' ["commit", "sha"] _String . getResponseBody
 gitHubArchive :: Text -> Text -> Text -> Text
 gitHubArchive owner repo rev =
   "https://github.com" <> foldMap ("/" <>) [owner, repo, "archive", rev <> ".tar.gz"]
+
+updateTime :: Value -> IO Value
+updateTime x = setTime <$> getTime <*> pure x
+
+getTime :: IO Day
+getTime = localDay <$> (utcToLocalTime <$> getCurrentTimeZone <*> getCurrentTime)
+
+setTime :: Day -> Value -> Value
+setTime x = over _Object (HashMap.insert "lastUpdated" (review _String (formatDay x)))
+
+formatDay :: Day -> Text
+formatDay = toText . formatTime defaultTimeLocale "%Y-%m-%d"
 
 forceKey :: Text -> Prism' Value a -> Value -> IO a
 forceKey k p v = orErr $ preview (key k % p) v
